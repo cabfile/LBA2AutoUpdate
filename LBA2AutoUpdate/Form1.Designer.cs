c237cd162f53d232f0ba803b0753580b95bf8fd0
.cab file <32414797+cabfile@users.noreply.github.com>
2024-10-17 15:01:07 +0300

    Add files via upload

---
+++ LBA2AutoUpdate/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace LBA2AutoUpdate {
+	partial class Form1 {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "One second...";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(217, 83);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Update";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Visible = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Enabled = false;
+			this.progressBar1.Location = new System.Drawing.Point(12, 54);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(277, 23);
+			this.progressBar1.TabIndex = 2;
+			this.progressBar1.Visible = false;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(304, 118);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "LittleBigAwoglet 2 Auto-Updater";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ LBA2AutoUpdate/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Policy;
+
+namespace LBA2AutoUpdate {
+	public partial class Form1 : Form {
+		const string endpoint = "http://cabfiel.tmxc.ru/";
+		private string hash;
+		private bool final;
+		private bool downloading;
+		private bool cancelling;
+		private System.Net.WebClient client;
+		private byte[] r;
+		private long lastBytes;
+		private long lastBytesSeen;
+		private string calcSpeed = "...";
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			if(!File.Exists("package.nw")) {
+				Final("Couldn't find package.nw.\nMake sure the auto-updater is in the same directory\nas the game.");
+				return;
+			}
+			System.Threading.Thread thread = new System.Threading.Thread(Stuff);
+			thread.Start();
+		}
+
+		public void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+			if(downloading && !cancelling && !final) {
+				SetStatus("Cancelling...");
+				cancelling = true;
+				client.CancelAsync();
+				e.Cancel = true;
+			}
+		}
+
+		private void Stuff() {
+			var md5 = System.Security.Cryptography.MD5.Create();
+			hash = BitConverter.ToString(md5.ComputeHash(File.ReadAllBytes("package.nw"))).Replace("-", "");
+			client = new System.Net.WebClient();
+			client.DownloadDataAsync(new Uri(endpoint + "meta.txt"));
+			client.DownloadDataCompleted += Client_DownloadDataCompleted;
+		}
+
+		private void Client_DownloadDataCompleted(object sender, System.Net.DownloadDataCompletedEventArgs e) {
+			if(e.Cancelled) {
+				Close();
+			}
+			else if(downloading) {
+				ProgressShow(false);
+				SetStatus("Writing...");
+				r = e.Result;
+				client.Dispose();
+				System.Threading.Thread thread = new System.Threading.Thread(Write);
+				thread.Start();
+			} else {
+				string[] d = Encoding.ASCII.GetString(e.Result).Split(';');
+				if(d[1] == hash) {
+					Final("You have the latest version.");
+				}
+				else {
+					SetStatus("An update (v" + d[0] + ") is available.\nClick Update to begin.");
+					ButtonShow(true);
+				}
+			}
+		}
+
+		private void Write() {
+			File.WriteAllBytes("package.nw", r);
+			Final("Update success!");
+		}
+
+		private void Final(string t) {
+			final = true;
+			SetStatus(t);
+			ButtonEnable(true);
+			ButtonShow(true);
+			ButtonText("OK");
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			if(final) Close();
+			else if(!downloading) {
+				downloading = true;
+				button1.Enabled = false;
+				SetStatus("Downloading...");
+				ProgressShow(true);
+				timer1.Start();
+				client.DownloadDataAsync(new Uri(endpoint + "package.nw"));
+				client.DownloadProgressChanged += Client_DownloadProgressChanged;
+			}
+		}
+
+		private void Client_DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e) {
+			if(cancelling) return;
+			ProgressValue(e.ProgressPercentage);
+			lastBytes = e.BytesReceived;
+			SetStatus("Downloading... " + PrettyPrintSize(e.BytesReceived) + " / " + PrettyPrintSize(e.TotalBytesToReceive) + "\n" + calcSpeed);
+		}
+		public static string PrettyPrintSize(float size, bool d = false) {
+			if(size < 1024) return size + " b";
+			else if(size < 1048576) return A(size / 1024,d) + " kb";
+			else if(size < 1073741824) return A(size / 1048576,d) + " mb";
+			else return A(size / 1073741824,d) + " gb";
+		}
+		public static string PrettyPrintSize(long size) {
+			return PrettyPrintSize((float)size);
+		}
+
+		private static float A(float x, bool d) => d ? (float)Math.Floor(x * 100) / 100 : (float)Math.Floor(x);
+
+		private void timer1_Tick(object sender, EventArgs e) {
+			calcSpeed = PrettyPrintSize(lastBytes - lastBytesSeen,true) + "/s";
+			lastBytesSeen = lastBytes;
+		}
+		// fuck winforms
+		private void SetStatus(string text) {
+			label1.BeginInvoke((MethodInvoker)(() => label1.Text = text));
+		}
+		private void ButtonShow(bool show) {
+			button1.BeginInvoke((MethodInvoker)(() => button1.Visible = show));
+		}
+		private void ButtonText(string text) {
+			button1.BeginInvoke((MethodInvoker)(() => button1.Text = text));
+		}
+		private void ButtonEnable(bool enable) {
+			button1.BeginInvoke((MethodInvoker)(() => button1.Enabled = enable));
+		}
+		private void ProgressShow(bool show) {
+			progressBar1.BeginInvoke((MethodInvoker)(() => progressBar1.Visible = show));
+		}
+		private void ProgressValue(int v) {
+			progressBar1.BeginInvoke((MethodInvoker)(() => progressBar1.Value = v));
+		}
+	}
+}
